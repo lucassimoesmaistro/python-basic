4c149da1468bc7f2122f8540d8cf628f108463a5
Lucas <lucassimoes_@hotmail.com>
2022-06-11 16:58:37 -0300

    tests

---
+++ basic.py
@@ -0,0 +1,7 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite idade: ')
+print('Hello ' + nome + ' idade ' + str(idade))
+print(pow(2, 3))
+
+nomes = ['nome1', 'nome2']
+print(nomes.index('nome1'))
